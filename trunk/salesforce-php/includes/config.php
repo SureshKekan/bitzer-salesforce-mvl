965131052ec1db117c5898cb309f30a45f353a21
uzaironbitzer@gmail.com <uzaironbitzer@gmail.com@9f1d4a1f-3119-ca01-b009-2d2f3eb0a8fd>
2011-08-19 06:33:03 +0000

    Dumping the new php mvl

--- trunk/salesforce-php/includes/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define("BASE_URL", "https://developer.bitzermobile.com/salesforce/");
-define("LIBRARY_IMAGE_URL", BASE_URL."sf.jpeg");
-$server_unique_key = "1";
-
-$salesforce_api_key = "<your SalesForce api key here";
-?>
